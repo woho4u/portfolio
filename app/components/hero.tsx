399cefc8f2705de2984de166abff45e05cfdae8b
Oliver Eriksen <okebedrift@gmail.com>
2024-05-14 13:18:23 +0200

    first commit

---
+++ app/components/hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <p>hei</p>
+      <img src="Redigert1.png" alt="" />
+    </div>
+  )
+}
+
+export default Hero
