ab1cc938ce9df7e3978f9df5ee0d3683efd79658
Oliver Eriksen <okebedrift@gmail.com>
2024-05-19 23:21:26 +0200

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero";
 import Header from "./components/header";
 import { useState } from "react";
 import StickyHeader from "./components/StickyHeader";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <div className="main-container eerie-background">
         <p className="h-screen">fa</p>
+        <Footer />
       </div>
     </>
   );
